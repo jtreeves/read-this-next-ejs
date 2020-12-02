17125a2f902aef15496bf5c544345a5117ac52e3
jtreeves <jackson.t.reeves@gmail.com>
2020-12-02 00:55:39 -0500

    [sql] Create models

---
+++ migrations/20201202053809-create-status.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('statuses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER
+      },
+      bookId: {
+        type: Sequelize.INTEGER
+      },
+      read: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('statuses');
+  }
+};
---
+++ models/pass.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class pass extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  pass.init({
+    userId: DataTypes.INTEGER,
+    bookId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'pass',
+  });
+  return pass;
+};
