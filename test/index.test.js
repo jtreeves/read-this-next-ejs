29fa5d4843a567e117684d401ee1473bf0b62611
jtreeves <jackson.t.reeves@gmail.com>
2020-12-07 16:40:05 -0500

    [tests] Delete semicolons, standardize indentation, and replace 'var' with 'const'

--- test/index.test.js
+++ test/index.test.js
@@ -1,9 +1,11 @@
-var expect = require('chai').expect;
-var request = require('supertest');
-var app = require('../server');
+const expect = require('chai').expect
+const request = require('supertest')
+const app = require('../server')
 
 describe('App', function() {
-  it('should return a 200 response', function(done) {
-    request(app).get('/').expect(200, done);
-  });
-});
+    it('should return a 200 response', function(done) {
+        request(app)
+            .get('/')
+            .expect(200, done)
+    })
+})
